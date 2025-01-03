abe82762288371f97b39d96de6b9f90d9f726ce4
Vrukshika <oadvrukshika@gmail.com>
2025-01-03 14:08:34 +0530

    adding files

---
+++ Functional-qns/Rectangle_Area.py
@@ -0,0 +1,5 @@
+
+l=int(input("Enter the length of the rectangle: "))
+h=int(input("Enter the height of the rectangle: "))
+area=l*h
+print("The area of the rectangle is: ",area)
---
+++ Functional-qns/celToFerTemp.py
@@ -0,0 +1,3 @@
+cal=int(input("Enter the temperature in Celsius: "))
+f=(cal*9/5)+32
+print("The temperature in Fahrenheit is: ",f)
