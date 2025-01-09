3bd8c0bd928de14a7c2e91351e76f87a7429a827
Vrukshika <oadvrukshika@gmail.com>
2025-01-09 14:00:28 +0530

    add searching and sorting

---
+++ Exception Handling/ExceptionHand.py
@@ -0,0 +1,15 @@
+try:
+    n = 0
+    res = 100 / n
+    
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+    
+except ValueError:
+    print("Enter a valid number!")
+    
+else:
+    print("Result is", res)
+    
+finally:
+    print("Execution complete.")
---
+++ Searching/BinarySearch.py
@@ -0,0 +1,23 @@
+#binary search
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    arr = [2, 3, 4, 10, 40]
+    target = 10
+    result = binary_search(arr, target)
+    print(result)
+   
+   
+   
+   
